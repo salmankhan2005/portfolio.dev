9fc4289c219d3df545efa1051207a47721fd5849
salmankhan <samitha0786@gmail.com>
2025-06-12 17:38:52 +0530

    push

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,8 +2,15 @@
 import Link from 'next/link';
 import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
+import dynamic from "next/dynamic";
+
+const MyComponent = dynamic(() => import("./MyComponent"), { ssr: false });
 
 function Footer() {
+  if (typeof window !== "undefined") {
+    // safe to use document or window here
+  }
+
   return (
     <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
       <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
