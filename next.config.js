eca057901ae2274c8463d98b4a0c5b7d13718fd4
salmankhan <samitha0786@gmail.com>
2025-06-14 00:53:11 +0530

    .

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path')
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
